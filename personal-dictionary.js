445d788dfb41e6c236c80c25ddee775186a8625b
Your Name <you@example.com>
2022-11-07 12:56:04 -0500

    personal dictionary

--- personal-dictionary.js
+++ personal-dictionary.js
@@ -1,12 +1,22 @@
-function(search){
+function search(){
+    // Go through all the elements of arr to look for item.
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] === item) { // Found it!
+        return i;
+      }
+    }
+    
+    // Item not found in the array.
+    return null;
+  }
+    
+
+
+function addWord(){
 
 }
 
-function(addWord){
-
-}
-
-int[] words = {};
+int[] words = [word1, word2, word3, word4];
 
 export let search
 export let addWord
